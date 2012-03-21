c0e82c56fcd6da2a8f7fdfc8f98ad832ff80d8ce
Buddy Lindsey <buddy@buddylindsey.com>
2012-03-20 23:22:14 -0500

    Changed all the things
    1) Added rotation on and off
    2) Added delete hammer. You can click delete button then every element you click after that will be deleted until you turn off the delete hammer
    3) added feature to bring the elements to the front
    4) Added feature for image to open up in same window.
    5) Changed rotateion to rotate 15 degrees instead of 45

--- coffee/javascript.coffee
+++ coffee/javascript.coffee
@@ -2,8 +2,23 @@ $(document).ready () ->
   stage = new Kinetic.Stage("canvas", 800, 800)
   layer = new Kinetic.Layer()
 
+  toRotate = false
+  toDelete = false
+
   rotate = (img) ->
-    img.rotate Math.PI / 4
+    if toRotate
+      img.rotate Math.PI / 12 
+      layer.draw()
+    return
+
+  remove = (img) ->
+    if toDelete
+      layer.remove(img)
+      layer.draw()
+    return
+
+  bringForward = (img) ->
+    layer.getChild(img.name).moveToTop()
     layer.draw()
     return
 
@@ -14,11 +29,14 @@ $(document).ready () ->
       y: 10,
       centerOffset: { x: this.width / 2, y: this.height / 2 },
       image: image,
-      draggable: true
+      draggable: true,
+      name: Math.random().toString()
       )
 
       img.on "mousedown", () ->
         rotate img
+        remove img
+        bringForward img
         return
 
       layer.add(img)
@@ -42,12 +60,40 @@ $(document).ready () ->
   $("#dresser").click () ->
     addImage($("#dresser").data("src")) 
     return
-  
+
+  $("#rotate").click () ->
+    if toRotate
+      toRotate = false
+      $("#rotate").html("Rotate off")
+      return
+    else
+      toRotate = true
+      $("#rotate").html("Rotate on")
+      return
+
+  $("#remove").click () ->
+    if toDelete
+      toDelete = false
+      $("#remove").html("Remove off")
+      return
+    else
+      toDelete = true
+      $("#remove").html("Remove on")
+      return
+   
   $("#save").click () ->
     stage.toDataURL (dataUrl) ->
       window.open(dataUrl)
       return
     , false
+    return
+
+  $("#toimage").click () ->
+    stage.toDataURL (dataUrl) ->
+        console.log(dataUrl)
+        $('#image').html("<img src='#{dataUrl}' />")
+        return
+      , false
     return
 
   return
--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,25 @@
 (function() {
 
   $(document).ready(function() {
-    var addImage, layer, rotate, stage;
+    var addImage, bringForward, layer, remove, rotate, stage, toDelete, toRotate;
     stage = new Kinetic.Stage("canvas", 800, 800);
     layer = new Kinetic.Layer();
+    toRotate = false;
+    toDelete = false;
     rotate = function(img) {
-      img.rotate(Math.PI / 4);
+      if (toRotate) {
+        img.rotate(Math.PI / 12);
+        layer.draw();
+      }
+    };
+    remove = function(img) {
+      if (toDelete) {
+        layer.remove(img);
+        layer.draw();
+      }
+    };
+    bringForward = function(img) {
+      layer.getChild(img.name).moveToTop();
       layer.draw();
     };
     addImage = function(src) {
@@ -21,10 +35,13 @@
             y: this.height / 2
           },
           image: image,
-          draggable: true
+          draggable: true,
+          name: Math.random().toString()
         });
         img.on("mousedown", function() {
           rotate(img);
+          remove(img);
+          bringForward(img);
         });
         layer.add(img);
         stage.add(layer);
@@ -43,9 +60,33 @@
     $("#dresser").click(function() {
       addImage($("#dresser").data("src"));
     });
+    $("#rotate").click(function() {
+      if (toRotate) {
+        toRotate = false;
+        $("#rotate").html("Rotate off");
+      } else {
+        toRotate = true;
+        $("#rotate").html("Rotate on");
+      }
+    });
+    $("#remove").click(function() {
+      if (toDelete) {
+        toDelete = false;
+        $("#remove").html("Remove off");
+      } else {
+        toDelete = true;
+        $("#remove").html("Remove on");
+      }
+    });
     $("#save").click(function() {
       stage.toDataURL(function(dataUrl) {
         window.open(dataUrl);
+      }, false);
+    });
+    $("#toimage").click(function() {
+      stage.toDataURL(function(dataUrl) {
+        console.log(dataUrl);
+        $('#image').html("<img src='" + dataUrl + "' />");
       }, false);
     });
   });
